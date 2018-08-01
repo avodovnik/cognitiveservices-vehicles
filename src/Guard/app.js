25e4791e90a852cd82913646bd98c8d518dbf24c
Mason Cusack <t-macusa@microsoft.com>
2018-08-01 13:23:00 +0100

    remote post endpoints from guard app

--- src/Guard/app.js
+++ src/Guard/app.js
@@ -5,32 +5,57 @@ var express = require('express');
 var path = require('path');
 var http = require('http');
 
+
 var app = express();
 
 var numplateregistered = "";
 var nprresultraw = "";
 var npverified = false; //true or false (has np been verified)
 var faceverified = false; //true or false (has it been verified)
+var currentlane;
 
-
-//allow post direct to app with API results
+/*allow post direct to app with API results
 app.post('/faceresult', function(request, response){
     
     //TODO: set faceresult raw = true if face is verified
     
-    console.log(request.body);      // your JSON
+    console.log(request.body);      //log to console for manual check
     response.send(request.body);    // echo the result back
 });  
+
+
+/*NPR result schema
+{
+    "Lane": 3,
+    "PlateContent": "V12LAF",
+    "RequestGuid": "8401eb6a-ead7-4887-82b9-6a45b8d36ed2"
+}
 
 app.post('/nprresult', function(request, response){
 
+    console.log(request);
+    //console.log(request.body); //log to console for manual check
+
+    
+    var request = JSON.parse(request);
+    //var body = JSON.parse(request.body);
+    console.log(request);
+    
+    currentlane = body.Lane;
+    nprresultraw = body.PlateContent;
+    //ignore request GUID for now
+
+    console.log(body);
+    console.log(currentlane);
+    console.log(nprresultraw);
+    
+
     npverified = (nprresultraw.includes(numplateregistered));
-
-    //TODO: set faceresult raw = true if face is verified
-
-    console.log(request.body);      // your JSON
-    response.send(request.body);    // echo the result back
+    //verify number plate
+    response.send({ status: 'SUCCESS' });
 });  
+
+*/
 
 function checkAPIresults(){
     if(faceverified == true && npverified == true){
