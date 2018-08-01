ca56a453f83354a7ee07c015a7c28229b9693588
Mason Cusack <t-macusa@microsoft.com>
2018-08-01 09:35:56 +0100

    npr image uploads working

--- src/NPR/app.js
+++ src/NPR/app.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 
 var express = require('express');
 var path = require('path');
+const multer = require("multer");
+const fs = require("fs");
+const http = require("http");
 
 var app = express();
 
@@ -13,5 +16,58 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
+
+app.get("/", express.static(path.join(__dirname, "./views")));
+
+const httpServer = http.createServer(app);
+
+const PORT = process.env.PORT || 4000;
+
+httpServer.listen(4000, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+const handleError = (err, res) => {
+    res
+      .status(500)
+      .contentType("text/plain")
+      .end("Oops! Something went wrong!");
+  };
+  
+  const upload = multer({
+    dest: "./uploads/"
+    // you might also want to set some limits: https://github.com/expressjs/multer#limits
+  });
+  
+  //handle form input (e.g uploads)
+  app.post(
+    "/upload",
+    upload.single("file" /* name attribute of <file> element in your form */),
+    (req, res) => {
+      const tempPath = req.file.path;
+      const targetPath = path.join(__dirname, "./uploads/image.jpg");
+  
+      if (path.extname(req.file.originalname).toLowerCase() === ".jpg") {
+        fs.rename(tempPath, targetPath, err => {
+          if (err) return handleError(err, res);
+  
+          res
+            .status(200)
+            .contentType("text/plain")
+            .end("File uploaded!");
+        });
+      } else {
+        fs.unlink(tempPath, err => {
+          if (err) return handleError(err, res);
+  
+          res
+            .status(403)
+            .contentType("text/plain")
+            .end("Only .jpg files are allowed!");
+        });
+      }
+    }
+  );
+  
 
 module.exports = app;
