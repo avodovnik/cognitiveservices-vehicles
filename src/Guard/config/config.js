3a5ae3df1347189f48a7dcf24d95d1d9c608f6b8
Anze Vodovnik <anze@vodovnik.com>
2018-08-01 11:29:54 +0100

    Azure Storage skeleton for guard app.

--- src/Guard/config/config.js
+++ src/Guard/config/config.js
@@ -1,2 +1,6 @@
 module.exports = {
-  };
+    storage: {
+        account: process.env.AZURE_STORAGE_ACCOUNT_NAME,
+        accessKey: process.env.AZURE_STORAGE_ACCOUNT_KEY
+    }
+};
--- src/Guard/routes/index.js
+++ src/Guard/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var config = require("../config/config.js");
+var storage = require('azure-storage');
 
 /***************************************************************************
     Home page
@@ -13,6 +14,16 @@ router.get('/', function(req, res, next) {
     res.render('index', {
         config: config
     });
+});
+
+router.get('/poll', function(req, res, next) {
+    // poll the queue, see if we have a message,
+    var retryOperations = new azure.ExponentialRetryPolicyFilter();
+    var queueSvc = azure.createQueueService(config.storage.account, config.storage.accessKey)
+                        .withFilter(retryOperations);
+
+    // if we do
+    // return the message as JSON
 });
 
 module.exports = router;
